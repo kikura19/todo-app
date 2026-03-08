fafc4715a67d66d8fdda119c8873a7b705959ee4
kikura19 <kakeru112828@icloud.com>
2026-03-08 22:09:17 +0900

    デザインの調整

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { AgGridModule } from 'ag-grid-angular';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,10 +17,15 @@ import { AgGridModule } from 'ag-grid-angular';
     TodoListComponent
   ],
   imports: [
+    BrowserAnimationsModule, // アニメーションを有効化
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    AgGridModule
+    AgGridModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
