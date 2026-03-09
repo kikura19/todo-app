eb1c9dfce0631128a8aadbb23ee89473e9942e7d
kikura19 <kakeru112828@icloud.com>
2026-03-09 22:59:23 +0900

    テストコードを一部実装

--- src/app/components/todo-list/todo-list.component.spec.ts
+++ src/app/components/todo-list/todo-list.component.spec.ts
@@ -1,23 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TodoListComponent } from './todo-list.component';
+import { TodoService } from 'src/app/services/todo.service';
+import { of } from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent;
   let fixture: ComponentFixture<TodoListComponent>;
+  let todoServiceSpy: jasmine.SpyObj<TodoService>;
+
+  // モック用のGridApiを用意
+  const mockGridApi = jasmine.createSpyObj('GridApi', [
+    'getSelectedRows', 
+    'refreshCells', 
+    'showLoadingOverlay', 
+    'hideOverlay', 
+    'startEditingCell'
+  ]);
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TodoListComponent ]
-    })
-    .compileComponents();
+    // TodoServiceのメソッドをスパイ（監視）できるようにする
+    const spy = jasmine.createSpyObj('TodoService', ['getTodos', 'deleteTodo', 'deleteTodosBatch', 'addTodo', 'updateTodo']);
 
-    fixture = TestBed.createComponent(TodoListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [TodoListComponent],
+      providers: [{ provide: TodoService, useValue: spy }]
+    }).compileComponents();
+
+    todoServiceSpy = TestBed.inject(TodoService) as jasmine.SpyObj<TodoService>;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TodoListComponent);
+    component = fixture.componentInstance;
+    
+    // GridApiを無理やりセットする（onGridReadyを呼ぶのと同じ状態にする）
+    (component as any).gridApi = mockGridApi; 
+  });
+
+  it('ngOnInit時にtodoService.getTodosが呼ばれるべき', () => {
+    todoServiceSpy.getTodos.and.returnValue(of([{ id: 1, task: 'テスト', isCompleted: false }]));
+    
+    component.ngOnInit();
+    
+    expect(todoServiceSpy.getTodos).toHaveBeenCalled();
+    expect(component.todos.length).toBe(1);
+  });
+
+  it('onDelete実行時に確認ダイアログが出て、削除APIが呼ばれるべき', () => {
+    // window.confirmをモック化
+    spyOn(window, 'confirm').and.returnValue(true);
+    todoServiceSpy.deleteTodo.and.returnValue(of());
+    // loadTodos内でgridApiを使うのでモック化
+    spyOn(component, 'loadTodos');
+
+    component.onDelete(1);
+
+    expect(window.confirm).toHaveBeenCalled();
+    expect(todoServiceSpy.deleteTodo).toHaveBeenCalledWith(1);
   });
 });
--- src/app/services/todo.service.spec.ts
+++ src/app/services/todo.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
   let service: TodoService;
+  let httpMock: HttpTestingController;
+
+
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TodoService]
+    });
     service = TestBed.inject(TodoService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
   });
+
+  it('getTodos()は正しいエンドポイントを叩くべき', ()=>{
+    const dummyTodos = [{id: 1,task:'テスト', isCompleted: false}];
+
+    service.getTodos().subscribe(data => {
+      expect(data).toEqual(dummyTodos);
+    });
+
+    const req = httpMock.expectOne(`http://localhost:8080/todos`);
+    expect(req.request.method).toBe('GET');
+
+    req.flush(dummyTodos);
+  });
+
+
 });
